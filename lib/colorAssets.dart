ad9fbd25f64d72ebef5b6c2ae1b83058dfec2dae
Melat t <melattesfaye16@gmail.com>
2024-03-29 00:38:45 +0300

    common pages and constants added

---
+++ lib/colorAssets.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorAssets {
+  static const primaryColor = Color(0xff006d77);
+  static const secondaryColor = Color(0xffa00ABB5);
+  static const lightSecondary = Color(0xffC2FFF3);
+  static const tertiaryColor = Color(0xffC2FFF3);
+  
+}
