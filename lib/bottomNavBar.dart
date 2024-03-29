7d6e3fa728441e419e720d7a156bbf0f9f62562d
Melat t <melattesfaye16@gmail.com>
2024-03-29 23:09:27 +0300

    bottom navigation bar completed

---
+++ lib/bottomNavBar.dart
@@ -0,0 +1,35 @@
+import 'package:bit_connect/colorAssets.dart';
+import 'package:curved_navigation_bar_with_label/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+
+
+
+
+class BottomBar extends StatefulWidget {
+  const BottomBar({super.key});
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ColorAssets.bduColor,
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: ColorAssets.bduColor,
+        items: [
+          CurvedNavigationBarItem(
+              icon: Icon(Icons.home, size: 30), label: "Home"),
+          CurvedNavigationBarItem(
+              icon: Icon(Icons.location_on, size: 30), label: "Sites"),
+          CurvedNavigationBarItem(
+              icon: Icon(Icons.food_bank_sharp, size: 30), label: "Cafe"),
+          CurvedNavigationBarItem(
+              icon: Icon(Icons.person, size: 30), label: "User"),
+        ],
+      ),
+    );
+  }
+}
--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,8 @@
+import 'package:curved_navigation_bar_with_label/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'colorAssets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'bottomNavBar.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -10,6 +14,64 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          appBar: PreferredSize(
+              preferredSize: const Size.fromHeight(200),
+              child: Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                  colors: [ColorAssets.bduColor, ColorAssets.lightSecondary],
+                  stops: [0.3, 1.0],
+                )),
+                child: SafeArea(
+                    child: Center(
+                  child: ListTile(
+                    leading: Icon(
+                      FontAwesomeIcons.bars,
+                      color: ColorAssets.white,
+                      size: 30,
+                    ),
+                  ),
+                )),
+              )),
+          bottomNavigationBar: CurvedNavigationBar(
+            backgroundColor: ColorAssets.bduColor,
+            items: [
+              CurvedNavigationBarItem(
+                  icon: Icon(
+                    Icons.home,
+                    size: 30,
+                    color: ColorAssets.bduColor,
+                  ),
+                  label: "Home"),
+              CurvedNavigationBarItem(
+                  icon: Icon(
+                    Icons.location_on,
+                    size: 30,
+                    color: ColorAssets.bduColor,
+                  ),
+                  label: "Sites"),
+              CurvedNavigationBarItem(
+                  icon: Icon(
+                    Icons.food_bank_sharp,
+                    size: 30,
+                    color: ColorAssets.bduColor,
+                  ),
+                  label: "Cafe"),
+              CurvedNavigationBarItem(
+                  icon: Icon(
+                    Icons.person,
+                    size: 30,
+                    color: ColorAssets.bduColor,
+                  ),
+                  label: "User"),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
