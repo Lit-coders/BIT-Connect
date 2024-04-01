8470a956faeaffd7b9bc526ed35acff0fea23d13
Melat t <melattesfaye16@gmail.com>
2024-04-01 13:16:00 +0300

    added onboarding images

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:bit_connect/home.dart';
+import 'package:bit_connect/onboarding/onbording.dart';
 import 'package:flutter/material.dart';
 import 'bottomNavBar.dart';
 
 void main() {
-  runApp(const Home());
+  runApp(const OnbordingScreen());
 }
--- lib/onboarding/onbording.dart
+++ lib/onboarding/onbording.dart
@@ -1,13 +1,49 @@
+import 'package:bit_connect/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_onboarding/flutter_onboarding.dart';
+
 
 class OnbordingScreen extends StatelessWidget {
-  const OnbordingScreen({super.key});
+  const OnbordingScreen({Key? key}) : super(key: key);
+
+List<IntroModel> pages(BuildContext context) {
+    return [
+      IntroModel(
+        title: Text(
+          'Excellent Student Experience',
+          style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                fontWeight: FontWeight.bold,
+              ),
+          textAlign: TextAlign.center,
+        ),
+        description: Text(
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+          textAlign: TextAlign.center,
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        image: Image.asset(
+          'assets/image.png',
+          fit: BoxFit.contain,
+          height: MediaQuery.of(context).size.height * 0.5,
+        ),
+      ),
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        
+    return Scaffold(
+      body: FlutterOnBoarding(
+       pages: pages,
+        onDone: () {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const Home(),
+            ),
+          );
+        },
       ),
     );
   }
