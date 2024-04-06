5c83940227c4d745b7c085f776edf6e6e4cedffe
Melat t <melattesfaye16@gmail.com>
2024-04-06 11:28:32 +0300

    custom appbar completed

--- lib/presentation/home.dart
+++ lib/presentation/home.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import '../utils/constants/colorAssets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -16,67 +15,59 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: SafeArea(
-          child: Scaffold(
-            backgroundColor: Colors.white,
-            appBar: PreferredSize(
-                preferredSize: const Size.fromHeight(200),
-                child: SafeArea(
-                    child: Padding(
-                  padding: const EdgeInsets.only(top: 25),
-                  child: ListTile(
-                    leading: IconButton(
-                      icon: Icon(
-                        FontAwesomeIcons.bars,
-                        color: ColorAssets.bduColor,
-                        size: 30,
-                      ),
-                      onPressed: () {
-                        Scaffold.of(context).openDrawer();
-                      },
-                    ),
-                  ),
-                ))),
-            body: Stack(
+        home: Scaffold(
+          backgroundColor: Colors.white,
+          body: SafeArea(
+            child: Column(
               children: [
-                Align(
-                  alignment: Alignment.topCenter,
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 20),
-                    child: Container(
-                      height: 150,
-                      width: 300,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        gradient: LinearGradient(
-                          colors: [
-                            ColorAssets.bduColor,
-                            ColorAssets.secondaryColor
-                          ],
-                        ),
+                //custom appbar
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 25, vertical: 25),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Image.asset(
+                        'assets/icons/menu.png',
+                        height: 45,
+                        color: ColorAssets.bduColor,
                       ),
-                    ),
+                      Image.asset(
+                        'assets/icons/person.png',
+                        height: 45,
+                        color: ColorAssets.bduColor,
+                      ),
+                    ],
                   ),
-                )
-              ],
-            ),
-            bottomNavigationBar: CurvedNavigationBar(
-              backgroundColor: Colors.white,
-              color: ColorAssets.bduColor,
-              items: <Widget>[
-                Icon(
-                  Icons.home,
-                  size: 30,
-                  color: ColorAssets.white,
                 ),
-                Icon(Icons.food_bank, size: 30, color: ColorAssets.white),
-                Icon(Icons.location_city, size: 30, color: ColorAssets.white),
-                Icon(Icons.person, size: 30, color: ColorAssets.white)
+                SizedBox(
+                  height: 20,
+                )
+                //Welcome
+
+                
+
+                //the 4 menus
               ],
-              onTap: (index) {
-                //Handle button tap
-              },
             ),
+          ),
+          bottomNavigationBar: CurvedNavigationBar(
+            height: 50,
+            backgroundColor: Colors.white,
+            color: ColorAssets.bduColor,
+            items: <Widget>[
+              Icon(
+                Icons.home,
+                size: 30,
+                color: ColorAssets.white,
+              ),
+              Icon(Icons.food_bank, size: 30, color: ColorAssets.white),
+              Icon(Icons.location_city, size: 30, color: ColorAssets.white),
+              Icon(Icons.person, size: 30, color: ColorAssets.white)
+            ],
+            onTap: (index) {
+              //Handle button tap
+            },
           ),
         ));
   }
