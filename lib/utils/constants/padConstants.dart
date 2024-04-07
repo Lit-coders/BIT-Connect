6d65084fd143db2568f67aa43075236ad3f8a0f9
Melat t <melattesfaye16@gmail.com>
2024-04-07 19:48:13 +0300

    The cafe menu option section created

--- lib/utils/constants/padConstants.dart
+++ lib/utils/constants/padConstants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class PaddingConstant {
-  static const double forPersonIcon = 40;
+  static const double forPersonIcon = 100;
   static const double horizontalPadding = 30;
   static const double verticalPadding = 20;
 }
