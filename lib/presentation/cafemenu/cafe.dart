01f605c5ff6be978d2949481155eab8c86e8946f
Melat t <melattesfaye16@gmail.com>
2024-04-07 20:12:34 +0300

    cafe widget

---
+++ lib/presentation/cafemenu/cafe.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CafeMenu extends StatefulWidget {
+  const CafeMenu({super.key});
+
+  @override
+  State<CafeMenu> createState() => _CafeMenuState();
+}
+
+class _CafeMenuState extends State<CafeMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
