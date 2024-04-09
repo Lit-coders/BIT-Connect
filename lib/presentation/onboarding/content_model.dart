84bd4dbf1325cd5eb24cfd7ac47eebac235a8894
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-04-09 11:45:44 -0700

    shorten the description as it will short statements

--- lib/presentation/onboarding/content_model.dart
+++ lib/presentation/onboarding/content_model.dart
@@ -12,20 +12,17 @@ List<OnboardingContent> contents = [
       image: 'assets/desinging.png',
       discription:
           "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the "
-          "industry's standard dummy text ever since the 1500s, "
-          "when an unknown printer took a galley of type and scrambled it "),
+     ),
   OnboardingContent(
-      title: 'Oncampus Refreshment',
+      title: 'Oncampus',
       image: 'assets/lunchtime.png',
       discription:
           "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the "
-          "industry's standard dummy text ever since the 1500s, "
-          "when an unknown printer took a galley of type and scrambled it "),
+    ),
   OnboardingContent(
       title: 'Campus Life',
       image: 'assets/studentsreading.png',
       discription:
           "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the "
-          "industry's standard dummy text ever since the 1500s, "
-          "when an unknown printer took a galley of type and scrambled it "),
+    ),
 ];
