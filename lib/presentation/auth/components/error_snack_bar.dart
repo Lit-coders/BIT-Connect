e95dfb0e0e7e3aa59d796e829585b62cb0830efd
sgc93 <smachewgedefaw@gmail.com>
2024-04-10 14:03:09 -0900

    create a snackbar to display error messeges

---
+++ lib/presentation/auth/components/error_snack_bar.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ErrorSnackBar {
+  final String content;
+  ErrorSnackBar({required this.content});
+
+  SnackBar getSnackBar() {
+    return SnackBar(
+      content: Text(
+        content,
+        style: const TextStyle(
+          fontSize: 17,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      duration: const Duration(seconds: 3),
+      behavior: SnackBarBehavior.floating,
+      margin: const EdgeInsets.only(
+        bottom: 300,
+        left: 20,
+        right: 20,
+      ),
+      backgroundColor: Colors.blue,
+    );
+  }
+}
