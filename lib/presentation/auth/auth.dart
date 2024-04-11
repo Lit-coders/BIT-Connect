ef9f7bb6af4e0ddf1bc974d2b0ad991a8af2f339
sgc93 <smachewgedefaw@gmail.com>
2024-04-10 15:15:13 -0900

    redirect user to specific page based on users action : login or sing up

--- lib/presentation/auth/auth.dart
+++ lib/presentation/auth/auth.dart
@@ -1,5 +1,8 @@
 import 'package:bit_connect/presentation/auth/login_page.dart';
 import 'package:bit_connect/presentation/auth/sign_up_screen.dart';
+import 'package:bit_connect/presentation/user_profile/abc.dart';
+import 'package:bit_connect/presentation/user_profile/new_user_profile.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Auth extends StatefulWidget {
@@ -20,10 +23,30 @@ class _AuthState extends State<Auth> {
 
   @override
   Widget build(BuildContext context) {
-    if (_showLoginPage) {
-      return Login(toggleToSignUP: toggleAuthScreens);
-    } else {
-      return SignUP(toggleToLogin: toggleAuthScreens);
-    }
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Scaffold(
+            body: Center(
+              child: Text('Error: ${snapshot.error}'),
+            ),
+          );
+        } else if (snapshot.hasData) {
+          print("stream data: ${snapshot.data}");
+          if (_showLoginPage) {
+            return const Abc();
+          } else {
+            return const BuildProfile();
+          }
+        } else {
+          if (_showLoginPage) {
+            return Login(toggleToSignUP: toggleAuthScreens);
+          } else {
+            return SignUP(toggleToLogin: toggleAuthScreens);
+          }
+        }
+      },
+    );
   }
 }
