b19737940c108d4b1a44080351da8fa12e9bdcc0
sgc93 <smachewgedefaw@gmail.com>
2024-04-11 02:17:56 -0900

    create root widget for the app

--- lib/bit_connect.dart
+++ lib/bit_connect.dart
@@ -1,6 +1,4 @@
 import 'package:bit_connect/presentation/auth/auth.dart';
-import 'package:bit_connect/presentation/user_profile/new_user_profile.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class BiTConnect extends StatelessWidget {
@@ -8,28 +6,8 @@ class BiTConnect extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            // Loading indicator if authentication state is being fetched
-            return const CircularProgressIndicator();
-          } else if (snapshot.hasError) {
-            // Handle error
-            return Scaffold(
-              body: Center(
-                child: Text('Error: ${snapshot.error}'),
-              ),
-            );
-          } else if (snapshot.hasData) {
-            // User is authenticated, show home page
-            return const BuildProfile();
-          } else {
-            return const Auth();
-          }
-        },
-      ),
+    return const MaterialApp(
+      home: Auth(),
     );
   }
 }
