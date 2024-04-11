3cf28ac4370a7e538839d30538fad649733e8bae
sgc93 <smachewgedefaw@gmail.com>
2024-04-11 02:16:27 -0900

    initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
-import 'package:bit_connect/presentation/auth/auth.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:io';
+
+import 'package:bit_connect/bit_connect.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const Auth());
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+            apiKey: "AIzaSyAZUj7DJ0vSNqkvIdGaI6E4O1j_eMp5Sxc",
+            appId: "1:880080912408:android:451d0acd1adf12a566036d",
+            messagingSenderId: "880080912408",
+            projectId: "bit-connect-7569f",
+          ),
+        )
+      : await Firebase.initializeApp();
+  runApp(const BiTConnect());
 }
