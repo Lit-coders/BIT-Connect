0ec9e06690fe197d5b03b36136f0d2f747f8492e
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-04-11 05:08:34 -0700

    adjust the starting point of the app

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,29 @@ import 'package:bit_connect/presentation/cafemenu/cafe.dart';
 import 'package:bit_connect/presentation/home.dart';
 import 'package:bit_connect/presentation/onboarding/onboard.dart';
 import 'package:flutter/material.dart';
-
+import 'package:bit_connect/presentation/onboarding/onboard.dart';
+import 'package:bit_connect/presentation/SplashScreen.dart';
 
 void main() {
-  runApp( CafeMenu());
+  runApp( 
+    const BitConnect(),
+  );
+}
+
+class BitConnect extends StatelessWidget {
+  const BitConnect({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Bit-Connect",
+      home: const SplashScreen(),
+      routes: {
+        '/home': (context) => const Home(), 
+        '/onboarding': (context) => const Onbording(), 
+        // '/cafe': (context) => const Cafe(), // 
+      },
+    );
+  }
 }
