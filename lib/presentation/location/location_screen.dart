ffa074e26e22b850e93f6c79588e716578cdb04b
sgc93 <smachewgedefaw@gmail.com>
2024-04-12 03:45:32 -0900

    build layout of location screen

--- lib/presentation/location/location_screen.dart
+++ lib/presentation/location/location_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bit_connect/searvices/helpers.dart';
 import 'package:flutter/material.dart';
 
 class Location extends StatefulWidget {
@@ -10,9 +11,38 @@ class Location extends StatefulWidget {
 class _LocationState extends State<Location> {
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Center(
-        child: Text("Map Page"),
+    return SafeArea(
+      child: Container(
+        width: getWidth(context),
+        height: getHeight(context),
+        color: Colors.blue[200],
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 150,
+              child: Text("Map Page"),
+            ),
+            Expanded(
+              child: Container(
+                width: getWidth(context),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                ),
+                child: const SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Text("Facilities"),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
