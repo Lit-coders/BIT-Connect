9c3621bd48ab959ea894bc00c351643408b67433
sgc93 <smachewgedefaw@gmail.com>
2024-04-11 23:28:46 -0900

    create a location screen widget

---
+++ lib/presentation/location/location_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Location extends StatefulWidget {
+  const Location({super.key});
+
+  @override
+  State<Location> createState() => _LocationState();
+}
+
+class _LocationState extends State<Location> {
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Center(
+        child: Text("Map Page"),
+      ),
+    );
+  }
+}
