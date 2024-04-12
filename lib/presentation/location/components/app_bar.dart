23494b2876a73597dd7a552011c8a220426aa4f8
sgc93 <smachewgedefaw@gmail.com>
2024-04-12 03:45:11 -0900

    fetch and display currently signed in user hignlight data

--- lib/presentation/location/components/app_bar.dart
+++ lib/presentation/location/components/app_bar.dart
@@ -1,20 +1,123 @@
+import 'package:bit_connect/searvices/helpers.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class LocationAppBar extends StatelessWidget implements PreferredSizeWidget {
+class LocationAppBar extends StatefulWidget {
   const LocationAppBar({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  State<LocationAppBar> createState() => _LocationAppBarState();
+}
+
+class _LocationAppBarState extends State<LocationAppBar> {
+  final _currentUser = FirebaseAuth.instance.currentUser;
+
+  Future<Map<String, dynamic>?> getUserData() async {
+    try {
+      final response = await FirebaseFirestore.instance
+          .collection("users")
+          .doc(_currentUser!.uid)
+          .get();
+      if (response.exists) {
+        final data = response.data();
+        return data;
+      } else {
+        return {
+          "fName":
+              _currentUser!.email!.split('@')[0].split(RegExp(r'[a-z]{3}'))[1],
+          "ppUrl": "",
+        };
+      }
+    } catch (error) {
+      return {
+        "fName":
+            _currentUser!.email!.split('@')[0].split(RegExp(r'[a-z]{3}'))[1],
+        "ppUrl": "",
+      };
+    }
+  }
+
+  Widget getPP(String ppUrl) {
+    return Container(
+      width: 60,
+      height: 60,
+      margin: const EdgeInsets.only(right: 10),
+      padding: const EdgeInsets.all(2),
+      decoration: const BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.all(Radius.circular(100)),
+      ),
+      child: Center(
+        child: ppUrl == ""
+            ? Text(
+                _currentUser!.email!
+                    .split('@')[0]
+                    .split(RegExp(r'[a-z]{3}'))[1]
+                    .substring(3),
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                  fontWeight: FontWeight.w500,
+                ),
+              )
+            : ClipRRect(
+                borderRadius: const BorderRadius.all(Radius.circular(100)),
+                child: Image(
+                  image: NetworkImage(ppUrl),
+                  fit: BoxFit.cover,
+                  height: 60,
+                  width: 60,
+                ),
+              ),
+      ),
+    );
+  }
+
+  AppBar getAppBar(Map<String, dynamic>? data) {
     return AppBar(
+      toolbarHeight: 80,
       automaticallyImplyLeading: false,
-      title: const ListTile(
-        title: Text("Hi, XYZ"),
-        subtitle: Text("what do you want to explore today?"),
+      backgroundColor: Colors.blue[200],
+      title: Container(
+        padding: const EdgeInsets.only(right: 5),
+        width: getWidth(context),
+        child: Row(
+          children: [
+            getPP(data == null ? "" : data['ppUrl']),
+            Column(
+              children: [
+                Text(
+                  'Hi, ${data == null ? _currentUser!.email!.split('@')[0].split(RegExp(r'[a-z]{3}'))[1] : data['fName']}',
+                  style: const TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+                const Text(
+                  "What do you want to exploare?",
+                  style: TextStyle(color: Colors.white, fontSize: 15),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
 
   @override
-  Size get preferredSize =>
-      Size(const Size.fromWidth(kToolbarHeight).width, 200);
+  Widget build(BuildContext context) {
+    return getAppBar(null);
+    // return FutureBuilder(
+    //   future: getUserData(),
+    //   builder: (context, snapshot) {
+    //     if (snapshot.connectionState == ConnectionState.waiting) {
+    //       return getAppBar(null);
+    //     } else if (snapshot.hasData || snapshot.hasError) {
+    //       final data = snapshot.requireData;
+    //       return getAppBar(data);
+    //     }
+    //     return const Center();
+    //   },
+    // );
+  }
 }
