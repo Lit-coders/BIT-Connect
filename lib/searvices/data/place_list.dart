9773b089c3b4e48c34c660f76b5df73fd1869069
sgc93 <smachewgedefaw@gmail.com>
2024-04-13 01:40:26 -0900

    create a stat data list of places

---
+++ lib/searvices/data/place_list.dart
@@ -0,0 +1,152 @@
+final List places = [
+  {
+    'category': 'Lounges',
+    'places': [
+      {
+        'name': 'Kitaw Lounge',
+        'description':
+            'a student\'s lounge named after a famous scientist and former student of BiT named Engineer Kitaw Ejigu.',
+      },
+      {
+        'name': 'Main BiT Lounge',
+        'description':
+            'a student\'s lounge named after a famous scientist and former student of BiT named Engineer Kitaw Ejigu.',
+      },
+      {
+        'name': 'Kitaw Lounge',
+        'description':
+            'a student\'s lounge named after a famous scientist and former student of BiT named Engineer Kitaw Ejigu.',
+      },
+      {
+        'name': 'Main BiT Lounge',
+        'description':
+            'a student\'s lounge named after a famous scientist and former student of BiT named Engineer Kitaw Ejigu.',
+      },
+      {
+        'name': 'Kitaw Lounge',
+        'description':
+            'a student\'s lounge named after a famous scientist and former student of BiT named Engineer Kitaw Ejigu.',
+      },
+      {
+        'name': 'Main BiT Lounge',
+        'description':
+            'a student\'s lounge named after a famous scientist and former student of BiT named Engineer Kitaw Ejigu.',
+      },
+      {
+        'name': 'Kitaw Lounge',
+        'description':
+            'a student\'s lounge named after a famous scientist and former student of BiT named Engineer Kitaw Ejigu.',
+      },
+      {
+        'name': 'Main BiT Lounge',
+        'description':
+            'a student\'s lounge named after a famous scientist and former student of BiT named Engineer Kitaw Ejigu.',
+      },
+    ]
+  },
+  {
+    'category': 'dormitories',
+    'places': [
+      {
+        'name': 'Kitaw Lounge',
+        'description':
+            'a student\'s lounge named after a famous scientist and former student of BiT named Engineer Kitaw Ejigu.',
+      },
+      {
+        'name': 'Main BiT Lounge',
+        'description':
+            'a student\'s lounge named after a famous scientist and former student of BiT named Engineer Kitaw Ejigu.',
+      }
+    ]
+  },
+  {
+    'category': 'Lounges',
+    'places': [
+      {
+        'name': 'Kitaw Lounge',
+        'description':
+            'a student\'s lounge named after a famous scientist and former student of BiT named Engineer Kitaw Ejigu.',
+      },
+      {
+        'name': 'Main BiT Lounge',
+        'description':
+            'a student\'s lounge named after a famous scientist and former student of BiT named Engineer Kitaw Ejigu.',
+      }
+    ]
+  },
+  {
+    'category': 'dormitories',
+    'places': [
+      {
+        'name': 'Kitaw Lounge',
+        'description':
+            'a student\'s lounge named after a famous scientist and former student of BiT named Engineer Kitaw Ejigu.',
+      },
+      {
+        'name': 'Main BiT Lounge',
+        'description':
+            'a student\'s lounge named after a famous scientist and former student of BiT named Engineer Kitaw Ejigu.',
+      }
+    ]
+  },
+  {
+    'category': 'Lounges',
+    'places': [
+      {
+        'name': 'Kitaw Lounge',
+        'description':
+            'a student\'s lounge named after a famous scientist and former student of BiT named Engineer Kitaw Ejigu.',
+      },
+      {
+        'name': 'Main BiT Lounge',
+        'description':
+            'a student\'s lounge named after a famous scientist and former student of BiT named Engineer Kitaw Ejigu.',
+      }
+    ]
+  },
+  {
+    'category': 'dormitories',
+    'places': [
+      {
+        'name': 'Kitaw Lounge',
+        'description':
+            'a student\'s lounge named after a famous scientist and former student of BiT named Engineer Kitaw Ejigu.',
+      },
+      {
+        'name': 'Main BiT Lounge',
+        'description':
+            'a student\'s lounge named after a famous scientist and former student of BiT named Engineer Kitaw Ejigu.',
+      }
+    ]
+  },
+  {
+    'category': 'Lounges',
+    'places': [
+      {
+        'name': 'Kitaw Lounge',
+        'description':
+            'a student\'s lounge named after a famous scientist and former student of BiT named Engineer Kitaw Ejigu.',
+      },
+      {
+        'name': 'Main BiT Lounge',
+        'description':
+            'a student\'s lounge named after a famous scientist and former student of BiT named Engineer Kitaw Ejigu.',
+      }
+    ]
+  },
+  {
+    'category': 'dormitories',
+    'places': [
+      {
+        'name': 'Kitaw Lounge',
+        'description':
+            'a student\'s lounge named after a famous scientist and former student of BiT named Engineer Kitaw Ejigu.',
+      },
+      {
+        'name': 'Main BiT Lounge',
+        'description':
+            'a student\'s lounge named after a famous scientist and former student of BiT named Engineer Kitaw Ejigu.',
+      }
+    ]
+  },
+];
