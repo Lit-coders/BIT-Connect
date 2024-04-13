cb830aef5ecd038047b5091f1fe96553828700ae
sgc93 <smachewgedefaw@gmail.com>
2024-04-13 10:57:31 -0900

    handle locating user in reusable way

--- lib/searvices/helpers.dart
+++ lib/searvices/helpers.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 double getWidth(context) {
   return MediaQuery.of(context).size.width;
@@ -6,4 +7,21 @@ double getWidth(context) {
 
 double getHeight(context) {
   return MediaQuery.of(context).size.height;
+}
+
+// getting user location
+// 1. to locate and mark devices's location
+// 2. to locate and mark nearest desired places from the device's current location
+
+Future<void> getUserLoc() async {
+  try {
+    await Geolocator.checkPermission();
+    await Geolocator.requestPermission();
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+    print(position);
+  } catch (error) {
+    print("error while locating user: $error");
+  }
 }
