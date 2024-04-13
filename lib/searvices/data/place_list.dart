e3f6013464ad1c69e9b5fb6290ff568272285358
sgc93 <smachewgedefaw@gmail.com>
2024-04-13 03:14:55 -0900

    add properties for places

--- lib/searvices/data/place_list.dart
+++ lib/searvices/data/place_list.dart
@@ -45,25 +45,24 @@ final List places = [
     ]
   },
   {
-    'category': 'Lounges',
+    'category': 'dormitories',
     'places': [
       {
-        'name': 'Kitaw Lounge',
+        'name': 'Dr. Aklilu Lama',
+        'description': 'a student\'s lounge named after a famous scientist.',
+      },
+      {
+        'name': 'Abdissa Aga',
         'description':
             'a student\'s lounge named after a famous scientist and former student of BiT named Engineer Kitaw Ejigu.',
       },
       {
-        'name': 'Main BiT Lounge',
+        'name': 'Mekele',
         'description':
             'a student\'s lounge named after a famous scientist and former student of BiT named Engineer Kitaw Ejigu.',
       },
       {
-        'name': 'Kitaw Lounge',
-        'description':
-            'a student\'s lounge named after a famous scientist and former student of BiT named Engineer Kitaw Ejigu.',
-      },
-      {
-        'name': 'Main BiT Lounge',
+        'name': 'Bilali habeshi',
         'description':
             'a student\'s lounge named after a famous scientist and former student of BiT named Engineer Kitaw Ejigu.',
       },
