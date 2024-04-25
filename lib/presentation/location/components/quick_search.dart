6022b87398ade6ddf047626b9aae8d875307e580
sgc93 <smachewgedefaw@gmail.com>
2024-04-25 07:18:29 -0900

    update contents fo quick search alert box

--- lib/presentation/location/components/quick_search.dart
+++ lib/presentation/location/components/quick_search.dart
@@ -1,15 +1,46 @@
 import 'package:flutter/material.dart';
 
-class QuickSearch extends StatefulWidget {
-  const QuickSearch({super.key});
+class QuickSearch {
+  List<Map<String, dynamic>>? searchResults;
+  static final fieldController = TextEditingController();
 
-  @override
-  State<QuickSearch> createState() => _QuickSearchState();
-}
+  static searchOnChange(value) {
+    print(value);
+  }
 
-class _QuickSearchState extends State<QuickSearch> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  static Future<dynamic> showQuickSearchWindow(BuildContext context) async {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Search places, facilities, ...'),
+          content: Column(
+            children: [
+              TextField(
+                controller: fieldController,
+                onChanged: searchOnChange,
+                autofocus: true,
+                style: const TextStyle(
+                  fontSize: 20,
+                ),
+                decoration: const InputDecoration(
+                  filled: true,
+                  fillColor: Color.fromARGB(25, 0, 0, 0),
+                  hintText: 'Search Places ...',
+                  hintStyle: TextStyle(
+                    color: Color.fromARGB(163, 0, 0, 0),
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
