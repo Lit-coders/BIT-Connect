01d57b9133ee0c1b2489b9a4394ed93ab1461129
sgc93 <smachewgedefaw@gmail.com>
2024-04-25 14:39:09 -0900

    define new state for search query to handle some state based functionalities

--- lib/presentation/location/model/search_result.dart
+++ lib/presentation/location/model/search_result.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 
 class Search extends ChangeNotifier {
   List<Map<String, dynamic>> searchResults = [];
+  String query = "";
 
   void updateSearchResults(List<Map<String, dynamic>> results) {
     searchResults = results;
+    notifyListeners();
+  }
+
+  void setQuery(query) {
+    query = query;
     notifyListeners();
   }
 }
