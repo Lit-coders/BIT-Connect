09b92981fb3a794ef97aed074cec2e7cd5cd47d8
sgc93 <smachewgedefaw@gmail.com>
2024-04-25 14:09:10 -0900

    move searching handler to separate file

--- lib/presentation/location/location_screen.dart
+++ lib/presentation/location/location_screen.dart
@@ -1,11 +1,10 @@
 import 'package:bit_connect/presentation/location/components/bit_map.dart';
 import 'package:bit_connect/presentation/location/components/loc_overview.dart';
-import 'package:bit_connect/presentation/location/model/search_result.dart';
+import 'package:bit_connect/presentation/location/components/quick_search.dart';
 import 'package:bit_connect/searvices/data/place_list.dart';
 import 'package:bit_connect/searvices/helpers.dart';
 import 'package:bit_connect/utils/constants/color_assets.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class Location extends StatefulWidget {
   const Location({super.key});
@@ -18,56 +17,6 @@ class _LocationState extends State<Location> {
   final _places = places;
   List _selectedPlace = places[0]['places'];
   int _selectedPlaceIndex = 0;
-
-  Future<dynamic> showQuickSearchWindow(context) async {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('Search places, facilities, ...'),
-          content: Column(
-            children: [
-              TextField(
-                onChanged: (query) {
-                  final result = searchFor(query);
-                  Search search = Provider.of<Search>(context, listen: false);
-                  search.updateSearchResults(result);
-                },
-                autofocus: true,
-                style: const TextStyle(
-                  fontSize: 20,
-                ),
-                decoration: const InputDecoration(
-                  filled: true,
-                  fillColor: Color.fromARGB(25, 0, 0, 0),
-                  hintText: 'Search Places ...',
-                  hintStyle: TextStyle(
-                    color: Color.fromARGB(163, 0, 0, 0),
-                  ),
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide.none,
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                  ),
-                  contentPadding: EdgeInsets.symmetric(horizontal: 10),
-                ),
-              ),
-              const Text('search results for ...'),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Consumer<Search>(builder: (context, search, child) {
-                  return Column(
-                    children: search.searchResults
-                        .map((place) => Text(place['name']))
-                        .toList(),
-                  );
-                }),
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
 
   Widget getPlaceTab(dynamic place, int index) {
     return GestureDetector(
@@ -105,7 +54,7 @@ class _LocationState extends State<Location> {
       child: SingleChildScrollView(
         child: GestureDetector(
           onTap: () => {
-            showQuickSearchWindow(context),
+            QuickSearch.showQuickSearchWindow(context),
           },
           child: Container(
             margin: const EdgeInsets.only(top: 70),
