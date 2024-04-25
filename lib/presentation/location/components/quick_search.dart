1a08ceedbe09184dff0a03768955a79ba3155444
sgc93 <smachewgedefaw@gmail.com>
2024-04-25 06:28:53 -0900

    create poping up searching window : with AlertWindow

---
+++ lib/presentation/location/components/quick_search.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuickSearch extends StatefulWidget {
+  const QuickSearch({super.key});
+
+  @override
+  State<QuickSearch> createState() => _QuickSearchState();
+}
+
+class _QuickSearchState extends State<QuickSearch> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
