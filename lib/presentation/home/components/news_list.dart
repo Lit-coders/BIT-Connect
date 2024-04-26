aa072993aa8626936b5f2d41d2f4176532a65eff
sgc93 <smachewgedefaw@gmail.com>
2024-04-26 13:03:39 -0900

    create and display sample news lists

---
+++ lib/presentation/home/components/news_list.dart
@@ -0,0 +1,123 @@
+import 'package:bit_connect/searvices/helpers.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class NewsList extends StatelessWidget {
+  NewsList({super.key});
+  final List<Map<String, dynamic>> newsList = [
+    {
+      'title': 'Who deserves A+? answer this and you will get A+',
+      'detail':
+          'the question is not how to get A+, it is who really deserves it. who really deserves it than Gash Dimamu!?',
+      'uploadTime': Timestamp.now(),
+      'img': 'assets/lunchtime.png',
+    },
+    {
+      'title': 'Who deserves A+?',
+      'detail':
+          'the question is not how to get A+, it is who really deserves it. who really deserves it than Gash Dimamu!?',
+      'uploadTime': Timestamp.now(),
+      'img': 'assets/bdu.png',
+    },
+    {
+      'title': 'Who deserves A+?',
+      'detail':
+          'the question is not how to get A+, it is who really deserves it. who really deserves it than Gash Dimamu!?',
+      'uploadTime': Timestamp.now(),
+      'img': 'assets/logo.png',
+    },
+    {
+      'title': 'Who deserves A+?',
+      'detail':
+          'the question is not how to get A+, it is who really deserves it. who really deserves it than Gash Dimamu!?',
+      'uploadTime': Timestamp.now(),
+      'img': 'assets/logo.png',
+    },
+    {
+      'title': 'Who deserves A+?',
+      'detail':
+          'the question is not how to get A+, it is who really deserves it. who really deserves it than Gash Dimamu!?',
+      'uploadTime': Timestamp.now(),
+      'img': 'assets/logo.png',
+    },
+    {
+      'title': 'Who deserves A+?',
+      'detail':
+          'the question is not how to get A+, it is who really deserves it. who really deserves it than Gash Dimamu!?',
+      'uploadTime': Timestamp.now(),
+      'img': 'assets/logo.png',
+    }
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: newsList.length,
+      itemBuilder: (context, index) {
+        final news = newsList[index];
+        final Timestamp uploadTime = news['uploadTime'];
+
+        return Container(
+          width: getWidth(context),
+          margin: const EdgeInsets.symmetric(horizontal: 18, vertical: 9),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Image(
+                  image: AssetImage(
+                    news['img'],
+                  ),
+                  fit: BoxFit.cover,
+                  width: 100,
+                  height: 100,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  height: 100,
+                  padding: const EdgeInsets.only(left: 9),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        newsList[index]['title'],
+                        overflow: TextOverflow.clip,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.timer,
+                            color: Colors.black38,
+                          ),
+                          Text(
+                            uploadTime
+                                .toDate()
+                                .toLocal()
+                                .toString()
+                                .split(" ")
+                                .toList()[0],
+                            style: const TextStyle(
+                                color: Colors.black38,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
