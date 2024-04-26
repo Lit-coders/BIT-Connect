036d5df15b5d1707cbde8a428b9bbac5cc1f96a6
sgc93 <smachewgedefaw@gmail.com>
2024-04-26 13:45:19 -0900

    update news slider style and composition

--- lib/presentation/home/components/news_slide.dart
+++ lib/presentation/home/components/news_slide.dart
@@ -1,6 +1,7 @@
 import 'package:bit_connect/searvices/helpers.dart';
 import 'package:bit_connect/utils/constants/color_assets.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class NewsSlide extends StatefulWidget {
@@ -24,55 +25,85 @@ class _NewsSlideState extends State<NewsSlide> {
         enlargeCenterPage: true,
       ),
       items: [
-        ["Welcome to BiT,", "Hey, LitCoders"],
-        ["News Poli", "Hot News"],
+        {
+          'title': 'Who deserves A+? answer this and you will get A+',
+          'detail':
+              'the question is not how to get A+, it is who really deserves it. who really deserves it than Gash Dimamu!?',
+          'uploadTime': Timestamp.now(),
+          'img': 'assets/lunchtime.png',
+        },
+        {
+          'title': 'Who deserves A+?',
+          'detail':
+              'the question is not how to get A+, it is who really deserves it. who really deserves it than Gash Dimamu!?',
+          'uploadTime': Timestamp.now(),
+          'img': 'assets/bdu.png',
+        },
         // Add more slides as needed
-      ].map((slide) {
+      ].map((Map<String, dynamic> slide) {
         return Builder(
           builder: (BuildContext context) {
             return Container(
               width: getWidth(context),
               margin: const EdgeInsets.symmetric(horizontal: 10.0),
-              decoration: const BoxDecoration(
+              decoration: BoxDecoration(
                 image: DecorationImage(
-                  image: AssetImage('assets/bdu.png'),
+                  image: AssetImage(slide['img']),
                   fit: BoxFit.cover,
                   opacity: 0.8,
                 ),
-                borderRadius: BorderRadius.all(Radius.circular(10)),
+                border: Border.all(color: ColorAssets.bduColor, width: 1),
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 15),
-                    child: Text(
-                      slide[1],
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                          backgroundColor: Colors.blue[100],
-                          fontSize: 25,
-                          fontWeight: FontWeight.w600,
-                          color: ColorAssets.white),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      left: 15,
-                      bottom: 15,
-                    ),
-                    child: Text(
-                      slide[0],
-                      textAlign: TextAlign.start,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w600,
-                        color: Colors.white,
-                        fontSize: 20,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(174, 144, 202, 249),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      margin: const EdgeInsets.only(bottom: 5),
+                      child: Text(
+                        slide['title'],
+                        overflow: TextOverflow.clip,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                    Container(
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(174, 144, 202, 249),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Row(
+                        children: [
+                          const Icon(
+                            Icons.timer,
+                            color: Colors.black54,
+                          ),
+                          Text(
+                            slide['uploadTime']
+                                .toDate()
+                                .toLocal()
+                                .toString()
+                                .split(" ")
+                                .toList()[0],
+                            style: const TextStyle(
+                                color: Colors.black54,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             );
           },
