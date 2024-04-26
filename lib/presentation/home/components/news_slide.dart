5d4da8a58793e0b72741bb03ba80b24799a97a89
sgc93 <smachewgedefaw@gmail.com>
2024-04-26 13:04:01 -0900

    update nes slide contents

--- lib/presentation/home/components/news_slide.dart
+++ lib/presentation/home/components/news_slide.dart
@@ -1,3 +1,4 @@
+import 'package:bit_connect/searvices/helpers.dart';
 import 'package:bit_connect/utils/constants/color_assets.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
@@ -14,7 +15,7 @@ class _NewsSlideState extends State<NewsSlide> {
   Widget build(BuildContext context) {
     return CarouselSlider(
       options: CarouselOptions(
-        height: 200.0,
+        height: 160.0,
         enableInfiniteScroll: true,
         autoPlay: true,
         autoPlayInterval: const Duration(seconds: 3),
@@ -30,33 +31,37 @@ class _NewsSlideState extends State<NewsSlide> {
         return Builder(
           builder: (BuildContext context) {
             return Container(
-              width: MediaQuery.of(context).size.width,
+              width: getWidth(context),
               margin: const EdgeInsets.symmetric(horizontal: 10.0),
               decoration: const BoxDecoration(
                 image: DecorationImage(
                   image: AssetImage('assets/bdu.png'),
                   fit: BoxFit.cover,
-                  // Adjust the fit as needed
+                  opacity: 0.8,
                 ),
                 borderRadius: BorderRadius.all(Radius.circular(10)),
               ),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.end,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Padding(
-                    padding: const EdgeInsets.only(left: 30),
+                    padding: const EdgeInsets.only(left: 15),
                     child: Text(
                       slide[1],
                       textAlign: TextAlign.start,
-                      style: const TextStyle(
-                          fontSize: 35,
+                      style: TextStyle(
+                          backgroundColor: Colors.blue[100],
+                          fontSize: 25,
                           fontWeight: FontWeight.w600,
                           color: ColorAssets.white),
                     ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.only(left: 30),
+                    padding: const EdgeInsets.only(
+                      left: 15,
+                      bottom: 15,
+                    ),
                     child: Text(
                       slide[0],
                       textAlign: TextAlign.start,
