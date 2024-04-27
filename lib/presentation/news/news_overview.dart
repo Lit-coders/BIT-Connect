eec2d03df72eb0db199857d02ac1c11421a90cc4
sgc93 <smachewgedefaw@gmail.com>
2024-04-27 03:31:43 -0900

    handle fetching news from the database and displaying it

--- lib/presentation/news/news_overview.dart
+++ lib/presentation/news/news_overview.dart
@@ -1,13 +1,12 @@
+import 'package:bit_connect/presentation/auth/components/error_snack_bar.dart';
 import 'package:bit_connect/presentation/home/components/user_welcomer.dart';
 import 'package:bit_connect/presentation/news/components/news_list.dart';
 import 'package:bit_connect/presentation/news/components/news_slide.dart';
-import 'package:bit_connect/presentation/news/model/news_model.dart';
 import 'package:bit_connect/searvices/helpers.dart';
+import 'package:bit_connect/utils/constants/color_assets.dart';
 import 'package:bit_connect/utils/constants/padding_constants.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:provider/provider.dart';
 
 class NewsOverview extends StatefulWidget {
   const NewsOverview({super.key});
@@ -17,36 +16,74 @@ class NewsOverview extends StatefulWidget {
 }
 
 class _NewsOverviewState extends State<NewsOverview> {
+  final CollectionReference news =
+      FirebaseFirestore.instance.collection('news');
+
+  Future<List<Map<String, dynamic>>> getNews() async {
+    List<Map<String, dynamic>> newses = [];
+    try {
+      final QuerySnapshot snapshot = await news.get();
+      for (var doc in snapshot.docs) {
+        newses.add(doc.data() as Map<String, dynamic>);
+      }
+      return newses;
+    } catch (e) {
+      final errorSnackBar =
+          ErrorSnackBar(content: 'Unable to fetch news, pleas try again');
+      if (mounted) errorSnackBar.getSnackBar();
+    }
+
+    return [];
+  }
+
   @override
   Widget build(BuildContext context) {
-    final newsModel = Provider.of<NewsModel>(context, listen: false);
-    newsModel.initializeNewsList();
-
     return SafeArea(
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(bottom: 8.0),
-            child: PreferredSize(
-              preferredSize: Size(getWidth(context), 100),
-              child: const UserWelcomer(),
-            ),
-          ),
-          const NewsSlide(),
-          Container(
-            width: getWidth(context),
-            padding: const EdgeInsets.all(8.0),
-            child: const Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: PaddingConstant.horizontalPadding),
-              child: Text(
-                "News",
-                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
+      child: FutureBuilder(
+        future: getNews(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(
+                color: ColorAssets.bduColor,
+                backgroundColor: Colors.white,
               ),
-            ),
-          ),
-          Expanded(child: NewsList())
-        ],
+            );
+          } else if (snapshot.hasError) {
+            return const Center(child: Text('Error'));
+          } else if (snapshot.hasData) {
+            final newsList = snapshot.requireData;
+            return Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: PreferredSize(
+                    preferredSize: Size(getWidth(context), 100),
+                    child: const UserWelcomer(),
+                  ),
+                ),
+                NewsSlide(newsList: newsList),
+                Container(
+                  width: getWidth(context),
+                  padding: const EdgeInsets.all(8.0),
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: PaddingConstant.horizontalPadding),
+                    child: Text(
+                      "News",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: NewsList(newsList: newsList),
+                )
+              ],
+            );
+          }
+          return const Center();
+        },
       ),
     );
   }
