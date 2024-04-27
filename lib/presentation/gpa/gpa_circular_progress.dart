075183774b0633ad8f4691bb080e271589f60105
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-04-27 13:43:08 -0700

    Refactor GpaCircularProgress widget to handle edge case when progressValue is 0

--- lib/presentation/gpa/gpa_circular_progress.dart
+++ lib/presentation/gpa/gpa_circular_progress.dart
@@ -9,23 +9,22 @@ class GpaCircularProgress extends StatelessWidget {
 
 
   const GpaCircularProgress({
-    Key? key,
     required this.gpa,
     this.size = 100.0,
     this.minGpa = 0.0,
     this.maxGpa = 4.0,
-
-  }) : super(key: key);
+    super.key
+  });
 
   @override
   Widget build(BuildContext context) {
-    double progressValue = (gpa - minGpa) / (maxGpa - minGpa);  
+    double progressValue = (gpa - minGpa) / (maxGpa - minGpa); 
 
     return SizedBox(
       width : size,
       height : size,
       child : CircularProgressIndicator (
-        value : progressValue,
+        value : progressValue == 0 ? 100.0 : progressValue,
         strokeWidth : 10,
         backgroundColor : Colors.grey[300],
           valueColor: AlwaysStoppedAnimation<Color>(
@@ -36,11 +35,11 @@ class GpaCircularProgress extends StatelessWidget {
   }
 
   Color _getColorForProgress(double progressValue) {
-    if (progressValue >= 0.75) {
+    if (progressValue >= 0.8) {
       return Colors.green;
-    } else if (progressValue >= 0.5) {
+    } else if (progressValue >= 0.6) {
       return Colors.yellow;
-    } else if (progressValue >= 0.25) {
+    } else if (progressValue >= 0.4) {
       return Colors.orange;
     } else {
       return Colors.red;
