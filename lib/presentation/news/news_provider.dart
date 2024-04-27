76457c607d1bd3d5cc47b6aaae71f919f8102df9
sgc93 <smachewgedefaw@gmail.com>
2024-04-27 03:30:36 -0900

    remove using provider

--- lib/presentation/news/news_provider.dart
+++ lib/presentation/news/news_provider.dart
@@ -1,16 +1,15 @@
-import 'package:bit_connect/presentation/news/model/news_model.dart';
 import 'package:bit_connect/presentation/news/news_overview.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class News extends StatelessWidget {
   const News({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => NewsModel(),
-      child: const NewsOverview(),
-    );
+    // return ChangeNotifierProvider(
+    //   create: (context) => NewsModel(),
+    //   child: const NewsOverview(),
+    // );
+    return const NewsOverview();
   }
 }
