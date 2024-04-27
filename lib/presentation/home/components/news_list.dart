bbe2cc6c00318ca86a29cecffacba81e4b2f30c1
sgc93 <smachewgedefaw@gmail.com>
2024-04-26 16:11:05 -0900

    handle detailling news from news list

--- lib/presentation/home/components/news_list.dart
+++ lib/presentation/home/components/news_list.dart
@@ -1,3 +1,4 @@
+import 'package:bit_connect/presentation/news/news_detail.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
@@ -56,65 +57,72 @@ class NewsList extends StatelessWidget {
         final news = newsList[index];
         final Timestamp uploadTime = news['uploadTime'];
 
-        return Card(
-          margin: const EdgeInsets.symmetric(horizontal: 18, vertical: 9),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(15),
-                  child: Image(
-                    image: AssetImage(
-                      news['img'],
+        return InkWell(
+          onTap: () => Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => NewsDetail(news: news),
+            ),
+          ),
+          child: Card(
+            margin: const EdgeInsets.symmetric(horizontal: 18, vertical: 9),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Image(
+                      image: AssetImage(
+                        news['img'],
+                      ),
+                      fit: BoxFit.cover,
+                      width: 90,
+                      height: 90,
                     ),
-                    fit: BoxFit.cover,
-                    width: 90,
-                    height: 90,
                   ),
-                ),
-                Expanded(
-                  child: Container(
-                    height: 90,
-                    padding: const EdgeInsets.only(left: 9),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text(
-                          newsList[index]['title'],
-                          overflow: TextOverflow.clip,
-                          style: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
+                  Expanded(
+                    child: Container(
+                      height: 90,
+                      padding: const EdgeInsets.only(left: 9),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            newsList[index]['title'],
+                            overflow: TextOverflow.clip,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                            ),
                           ),
-                        ),
-                        Row(
-                          children: [
-                            const Icon(
-                              Icons.timer,
-                              color: Colors.black38,
-                            ),
-                            Text(
-                              uploadTime
-                                  .toDate()
-                                  .toLocal()
-                                  .toString()
-                                  .split(" ")
-                                  .toList()[0],
-                              style: const TextStyle(
-                                  color: Colors.black38,
-                                  fontWeight: FontWeight.w500),
-                            ),
-                          ],
-                        ),
-                      ],
+                          Row(
+                            children: [
+                              const Icon(
+                                Icons.timer,
+                                color: Colors.black38,
+                              ),
+                              Text(
+                                uploadTime
+                                    .toDate()
+                                    .toLocal()
+                                    .toString()
+                                    .split(" ")
+                                    .toList()[0],
+                                style: const TextStyle(
+                                    color: Colors.black38,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                )
-              ],
+                  )
+                ],
+              ),
             ),
           ),
         );
