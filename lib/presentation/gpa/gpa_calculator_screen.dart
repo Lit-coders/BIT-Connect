8aa389be8d312a99a08f9ae0c514bb75d3d124b1
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-04-26 17:46:39 -0700

    add dashed border to Add course button

--- lib/presentation/gpa/gpa_calculator_screen.dart
+++ lib/presentation/gpa/gpa_calculator_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dotted_border/dotted_border.dart';
 
 class GpaCalculatorScreen extends StatefulWidget {
   GpaCalculatorScreen({Key? key}) : super(key: key);
@@ -35,13 +36,49 @@ class _GpaCalculatorScreenState extends State<GpaCalculatorScreen> {
                 },
               ),
             ),
-            ElevatedButton(
+            InkWell(
+              onTap: () {
+                setState(() {
+                  courseData.add({});
+                });
+              },
+              child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: DottedBorder(
+                    dashPattern: [5,5],
+                    color: Colors.grey,
+                    strokeWidth : 2,
+                    child: Expanded(
+                      child: Container(
+                        // decoration: BoxDecoration(
+                        //   border: Border.all(color: Colors.blueAccent),
+                        // ),
+                          padding : const EdgeInsets.all(20),
+                          child: const Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.add_circle,
+                                color: Colors.red,
+                                size: 20,
+                              ),
+                              SizedBox(width: 10),
+                              Text(
+                                'Add Course',
+                              )
+                            ],
+                          )),
+                    ),
+                  )),
+            ),
+            const SizedBox(height: 40),
+            OutlinedButton(
               onPressed: () {
                 setState(() {
                   courseData.add({});
                 });
               },
-              child: const Text('Add Course'),
+              child: const Text('Calculate GPA'),
             ),
           ],
         ),
@@ -62,66 +99,71 @@ class CourseInputRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(15.0),
-      child:
-      Container (
-       decoration : BoxDecoration(
-          border: Border.all(color: Colors.blueAccent)
-       ),
-       child :Column(
-        children: [
-          TextField(
-              onChanged: (value) {
-                courseData['courseName'] = value;
-                onCourseDataChanged(courseData);
-              },
-              decoration: const InputDecoration(
-                hintText: 'Course Name',
-                border: UnderlineInputBorder(),
-
-              ),
-            ),
-          Row(children: [
-            Expanded(
-              child: DropdownButtonFormField<String>(
-                value: courseData['grade'],
-                onChanged: (newValue) {
-                  courseData['grade'] = newValue!;
+        padding: const EdgeInsets.all(10.0),
+        child: Container(
+          decoration:
+              BoxDecoration(border: Border.all(color: Colors.blueAccent)),
+          child: Column(
+            children: [
+              TextField(
+                onChanged: (value) {
+                  courseData['courseName'] = value;
                   onCourseDataChanged(courseData);
                 },
-                items: <String>['A', 'B', 'C'].map((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
                 decoration: const InputDecoration(
-                  hintText: 'Grade',
+                  contentPadding: EdgeInsets.all(10),
+                  hintText: 'Course Name',
+                  border: UnderlineInputBorder(),
                 ),
               ),
-            ),
-            Expanded(
-              child: DropdownButtonFormField<int>(
-                value: courseData['value'],
-                onChanged: (newValue) {
-                  courseData['value'] = newValue!;
-                  onCourseDataChanged(courseData);
-                },
-                items: <int>[4, 3, 2, 1].map((int value) {
-                  return DropdownMenuItem<int>(
-                    value: value,
-                    child: Text(value.toString()),
-                  );
-                }).toList(),
-                decoration: const InputDecoration(
-                  hintText: 'Value',
+              Row(children: [
+                Expanded(
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        border: Border(
+                      right: BorderSide(color: Colors.black),
+                    )),
+                    child: DropdownButtonFormField<String>(
+                      value: courseData['grade'],
+                      onChanged: (newValue) {
+                        courseData['grade'] = newValue!;
+                        onCourseDataChanged(courseData);
+                      },
+                      items: <String>['A', 'B', 'C'].map((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      decoration: const InputDecoration(
+                          contentPadding: EdgeInsets.all(10),
+                          hintText: 'Grade',
+                          border: InputBorder.none),
+                    ),
+                  ),
                 ),
-              ),
-            ),
-          ])
-        ],
-      ),
-      )
-    );
+                Expanded(
+                  child: DropdownButtonFormField<int>(
+                    value: courseData['value'],
+                    onChanged: (newValue) {
+                      courseData['value'] = newValue!;
+                      onCourseDataChanged(courseData);
+                    },
+                    items: <int>[4, 3, 2, 1].map((int value) {
+                      return DropdownMenuItem<int>(
+                        value: value,
+                        child: Text(value.toString()),
+                      );
+                    }).toList(),
+                    decoration: const InputDecoration(
+                      hintText: 'Value',
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ])
+            ],
+          ),
+        ));
   }
 }
