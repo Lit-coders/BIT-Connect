9a736b280bf312bc5468b64a220275cdf6dc21e3
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-04-28 03:33:23 -0700

    Refactor GpaCalculatorScreen to use GpaConstants for grade values and credit points

--- lib/presentation/gpa/gpa_calculator.dart
+++ lib/presentation/gpa/gpa_calculator.dart
@@ -1,26 +1,13 @@
-Map<String, double> gradeValue = {
-      'A': 4,
-      'A-': 3.75,
-      'B+': 3.5,
-      'B': 3,
-      'B-': 2.75,
-      'C+': 2.5,
-      'C': 2,
-      'C-': 1.75,
-      'D': 1,
-      'F': 0,
-  }; 
-List <int> creditPoints = [30,24,7,6,5,4,3,0];
- 
- 
-  double calculateGpa(List<Map<String, dynamic>> courseData) {
-    double totalValue = 0;
-    double totalWeight = 0;
-    for (var course in courseData) {
-      if (course['value'] != null || course['grade'] != null) {
-        totalValue += (course['value'] as double) * gradeValue[course['grade']]!;
-        totalWeight += course['value'] as double;
-      }
+import 'package:bit_connect/utils/constants/gpa_constants.dart';
+double calculateGpa(List<Map<String, dynamic>> courseData) {
+  GpaConstants gpaConstants = GpaConstants();
+  double totalValue = 0;
+  double totalWeight = 0;
+  for (var course in courseData) {
+    if (course['value'] != null || course['grade'] != null) {
+      totalValue += (course['value'] as double) * gpaConstants.gradeValue[course['grade']]!;
+      totalWeight += course['value'] as double;
     }
-    return totalWeight == 0 ? 0 : totalValue / totalWeight;
   }
+  return totalWeight == 0 ? 0 : totalValue / totalWeight;
+}
