671d9dffaf8ae3965c398f08a4485f0470124fca
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-04-28 03:33:37 -0700

    Refactor GpaConstants to store grade values and credit points

---
+++ lib/utils/constants/gpa_constants.dart
@@ -0,0 +1,15 @@
+class GpaConstants {
+  Map<String, double> gradeValue = {
+    'A': 4,
+    'A-': 3.75,
+    'B+': 3.5,
+    'B': 3,
+    'B-': 2.75,
+    'C+': 2.5,
+    'C': 2,
+    'C-': 1.75,
+    'D': 1,
+    'F': 0,
+  };
+  List<int> creditPoints = [30, 24, 7, 6, 5, 4, 3, 0];
+}
