0ab608351ef9a6308bfe0877022227ea7c612adf
sgc93 <smachewgedefaw@gmail.com>
2024-04-28 09:22:49 -0900

    handle save login data to shared preference

--- lib/presentation/sims/provider/sims_provider.dart
+++ lib/presentation/sims/provider/sims_provider.dart
@@ -1,8 +1,11 @@
+import 'package:bit_connect/presentation/sims/model/sims_std.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SIMSProvider extends ChangeNotifier {
   int _previousIndex = 0;
   bool _isLoginCanceled = false;
+  Student? student;
 
   void setPreviousIndex(int index) {
     _previousIndex = index;
@@ -21,4 +24,15 @@ class SIMSProvider extends ChangeNotifier {
 
   int get previousIndex => _previousIndex;
   bool get isLoginCanceled => _isLoginCanceled;
+
+  // manage Account
+  Future<void> login(username, password) async {
+    try {
+      SharedPreferences loginPref = await SharedPreferences.getInstance();
+      loginPref.setString('username', username);
+      loginPref.setString('username', password);
+    } catch (e) {
+      // let's hope the error will not happen next time
+    }
+  }
 }
