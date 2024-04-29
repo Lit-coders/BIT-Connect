2a43457982d885dd39f7d565b32d0bde6acc8469
sgc93 <smachewgedefaw@gmail.com>
2024-04-29 13:52:28 -0900

    fix conflict

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,13 +1,14 @@
 import 'package:bit_connect/common/app_bar.dart';
 import 'package:bit_connect/presentation/gpa/gpa_calculator_screen.dart';
-import 'package:bit_connect/presentation/home/components/app_bar.dart';
 import 'package:bit_connect/presentation/home/components/left_drawer.dart';
 import 'package:bit_connect/presentation/location/location_screen.dart';
 import 'package:bit_connect/presentation/news/news_provider.dart';
 import 'package:bit_connect/presentation/sims/components/sims_login.dart';
+import 'package:bit_connect/presentation/sims/provider/sims_provider.dart';
 import 'package:bit_connect/utils/constants/color_assets.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -32,6 +33,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+    final simsProvider = Provider.of<SIMSProvider>(context, listen: false);
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -40,9 +42,13 @@ class _HomeState extends State<Home> {
         appBar: appBarChooser(_contentIndex, context, scaffoldKey),
         backgroundColor: Colors.white,
         drawer: const LeftDrawer(),
-        body: homeContents[_contentIndex],
+        body: homeContents[simsProvider.isLoginCanceled
+            ? simsProvider.previousIndex
+            : _contentIndex],
         bottomNavigationBar: CurvedNavigationBar(
-          index: _contentIndex,
+          index: simsProvider.isLoginCanceled
+              ? simsProvider.previousIndex
+              : _contentIndex,
           height: 50,
           backgroundColor: Colors.transparent,
           color: ColorAssets.bduColor,
