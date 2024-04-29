3384fdf16ab484395bcd721f8c296984adf31085
sgc93 <smachewgedefaw@gmail.com>
2024-04-28 23:37:34 -0900

    display sims content conditionally

--- lib/presentation/sims/sims.dart
+++ lib/presentation/sims/sims.dart
@@ -1,21 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class SIMS extends StatefulWidget {
-  const SIMS({super.key});
+  bool isWebView;
+
+  SIMS({super.key, required this.isWebView});
 
   @override
   State<SIMS> createState() => _SIMSState();
 }
 
 class _SIMSState extends State<SIMS> {
+  late final WebViewController controller;
+
+  var loadingPercentage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setNavigationDelegate(NavigationDelegate(
+        onPageStarted: (url) {
+          setState(() {
+            loadingPercentage = 0;
+          });
+        },
+        onProgress: (progress) {
+          setState(() {
+            loadingPercentage = progress;
+          });
+        },
+        onPageFinished: (url) {
+          setState(() {
+            loadingPercentage = 100;
+          });
+        },
+      ))
+      ..loadRequest(
+        Uri.parse('https://bdu.edu.et/'),
+      );
+  }
+
+  Widget bitWebView() {
+    return Stack(
+      children: [
+        WebViewWidget(
+          controller: controller,
+        ),
+        if (loadingPercentage < 100)
+          LinearProgressIndicator(
+            value: loadingPercentage / 100.0,
+          ),
+      ],
+    );
+  }
+
+  Widget userData() {
+    return const Center(
+      child: Text("User Data"),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Stack(
-        children: [
-          Text("abc"),
-        ],
-      ),
+    return Scaffold(
+      appBar: AppBar(),
+      body: widget.isWebView ? bitWebView() : userData(),
     );
   }
 }
