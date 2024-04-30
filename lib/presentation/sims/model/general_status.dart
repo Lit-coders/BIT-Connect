f0e3c27650bc2d687447e5e4de868bedc77de257
sgc93 <smachewgedefaw@gmail.com>
2024-04-30 10:56:17 -0900

    create general status model class

---
+++ lib/presentation/sims/model/general_status.dart
@@ -0,0 +1,23 @@
+class GeneralStatus {
+  String academicYear;
+  String semester;
+  String batch;
+  String regDate;
+  String regCondition;
+  String sGpa;
+  double cGpa;
+  double prevStatus;
+  double finalStatus;
+
+  GeneralStatus({
+    required this.academicYear,
+    required this.semester,
+    required this.batch,
+    required this.regDate,
+    required this.regCondition,
+    required this.sGpa,
+    required this.cGpa,
+    required this.prevStatus,
+    required this.finalStatus,
+  });
+}
