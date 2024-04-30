1c277ebdceb4dd434cd2c29bfeb1d5aceb0b9df9
sgc93 <smachewgedefaw@gmail.com>
2024-04-30 12:57:43 -0900

    handle fetching data : general status

--- lib/presentation/sims/api/sims_general_status.dart
+++ lib/presentation/sims/api/sims_general_status.dart
@@ -1,8 +1,15 @@
+import 'dart:convert';
+
+import 'package:bit_connect/presentation/sims/model/general_status.dart';
 import 'package:bit_connect/utils/constants/api_constants.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
-Future<http.Response> fetchGeneralStatus(String token) async {
+Future<void> fetchGeneralStatus() async {
   try {
+    SharedPreferences stdPref = await SharedPreferences.getInstance();
+    final token = stdPref.getString('simsToken');
+
     final response = await http.get(
       Uri.parse(GENERAL_STATUS_ENDPOINT),
       headers: {
@@ -11,9 +18,18 @@ Future<http.Response> fetchGeneralStatus(String token) async {
       },
     );
 
-    print(response.body);
-
-    return response;
+    final Map<String, dynamic> body = jsonDecode(response.body);
+    final int statusCode = response.statusCode;
+    if (statusCode == 200) {
+      final List<Map<String, dynamic>> statusResult = body['generalStatus'];
+      final List<GeneralStatus> generalStatus = [];
+      print(generalStatus);
+      //  generalStatus.addAll(statusResult.map((e) => )) ;
+    } else {
+      if (body['error'].isNotEmpty) {
+        print(body);
+      }
+    }
   } catch (e) {
     rethrow;
   }
