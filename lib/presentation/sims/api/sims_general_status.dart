2f1c9ad26a43e7a36af2dab59d871d0e36f660d2
sgc93 <smachewgedefaw@gmail.com>
2024-04-29 21:52:39 -0900

    clear db related lines

--- lib/presentation/sims/api/sims_general_status.dart
+++ lib/presentation/sims/api/sims_general_status.dart
@@ -1,19 +1,17 @@
 import 'package:bit_connect/utils/constants/api_constants.dart';
 import 'package:http/http.dart' as http;
-import 'package:shared_preferences/shared_preferences.dart';
 
-Future<http.Response> fetchGeneralStatus() async {
+Future<http.Response> fetchGeneralStatus(String token) async {
   try {
-    final SharedPreferences loginPref = await SharedPreferences.getInstance();
-    final String? token = loginPref.getString('simsToke');
-
     final response = await http.get(
       Uri.parse(GENERAL_STATUS_ENDPOINT),
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer ${token ?? ''}',
+        'Authorization': 'Bearer $token',
       },
     );
+
+    print(response.body);
 
     return response;
   } catch (e) {
