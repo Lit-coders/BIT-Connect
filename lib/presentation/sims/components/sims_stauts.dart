c26417854504cc4f17c33f903e6a9dcac246d7b6
sgc93 <smachewgedefaw@gmail.com>
2024-04-29 17:00:22 -0900

    create file to display student status

---
+++ lib/presentation/sims/components/sims_stauts.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SIMSStatus extends StatefulWidget {
+  bool isWebView;
+  SIMSStatus({super.key, required this.isWebView});
+
+  @override
+  State<SIMSStatus> createState() => _SIMSStatusState();
+}
+
+class _SIMSStatusState extends State<SIMSStatus> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
