c6bb07c04a51cfb82649290b3ff6968bca3d5d66
sgc93 <smachewgedefaw@gmail.com>
2024-04-29 18:09:37 -0900

    clear logger function

--- lib/presentation/sims/api/sims_auth.dart
+++ lib/presentation/sims/api/sims_auth.dart
@@ -5,19 +5,16 @@ import 'package:http/http.dart' as http;
 
 Future<http.Response> loginSIMS(username, password) async {
   try {
-    // Create a map to hold the data
-
     final data = {
       'username': username,
       'password': password,
     };
 
-    // Convert the map to a JSON string
     final jsonData = jsonEncode(data);
 
-    // Send the POST request with the sdJSON body
     final response = await http.post(Uri.parse(SIMS_LOGIN_ENDPOINT),
         body: jsonData, headers: {'Content-Type': 'application/json'});
+
     return response;
   } catch (e) {
     rethrow;
