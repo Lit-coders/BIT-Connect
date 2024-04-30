158d3ff7974dc94640317886970aba68faa474c5
sgc93 <smachewgedefaw@gmail.com>
2024-04-29 17:56:15 -0900

    handle redirecting logged in user to status page directly

--- lib/presentation/sims/sims.dart
+++ lib/presentation/sims/sims.dart
@@ -1,5 +1,7 @@
 import 'package:bit_connect/presentation/sims/components/sims_login.dart';
+import 'package:bit_connect/presentation/sims/components/sims_stauts.dart';
 import 'package:bit_connect/presentation/sims/components/sims_webView.dart';
+import 'package:bit_connect/presentation/sims/helpers/sims_helpers.dart';
 import 'package:bit_connect/presentation/sims/provider/sims_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -16,6 +18,10 @@ class _SIMSState extends State<SIMS> {
   Widget build(BuildContext context) {
     return Consumer<SIMSProvider>(
       builder: ((context, simsProvider, child) {
+        initializeLoginPreference(simsProvider);
+        if (simsProvider.isUserLoggedInBefore) {
+          return const SIMSStatus();
+        }
         return Scaffold(
           body: simsProvider.isStatusWithLogin
               ? const SIMSLogin()
