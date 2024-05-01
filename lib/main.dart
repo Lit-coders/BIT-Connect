f73577f5c774e9ba1fa4c257546e4aadc91c58ff
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-05-01 22:43:21 +0300

    refactor: remove firebase credentials for security purposes

--- lib/main.dart
+++ lib/main.dart
@@ -7,20 +7,23 @@ import 'package:bit_connect/presentation/news/model/news_model.dart';
 import 'package:bit_connect/presentation/onboarding/onboarding_screen.dart';
 import 'package:bit_connect/presentation/sims/provider/sims_provider.dart';
 import 'package:bit_connect/presentation/splash_screen.dart';
+import 'package:bit_connect/utils/load_env.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:provider/provider.dart';
 
 Future<void> main() async {
+  await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
   Platform.isAndroid
       ? await Firebase.initializeApp(
-          options: const FirebaseOptions(
-            apiKey: "AIzaSyAZUj7DJ0vSNqkvIdGaI6E4O1j_eMp5Sxc",
-            appId: "1:880080912408:android:451d0acd1adf12a566036d",
-            messagingSenderId: "880080912408",
-            projectId: "bit-connect-7569f",
+          options: FirebaseOptions(
+            apiKey: loadFromEnv("FIREBASE_API_KEY"),
+            appId: loadFromEnv("FIREBASE_APP_ID"),
+            messagingSenderId: loadFromEnv("FIREBASE_MESSAGING_SENDER_ID"),
+            projectId: loadFromEnv("FIREBASE_PROJECT_ID"),
           ),
         )
       : await Firebase.initializeApp();
