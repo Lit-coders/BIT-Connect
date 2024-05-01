f507e638df1f93ebdecfcf54237366195aa9ab69
sgc93 <smachewgedefaw@gmail.com>
2024-05-01 04:32:03 -0900

    create widget to handle dev list

---
+++ lib/presentation/home/components/developers_list.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class DevList extends StatelessWidget {
+  const DevList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
