6ddefceed789d695f8a68673c6a90a57ac808d39
sgc93 <smachewgedefaw@gmail.com>
2024-04-30 16:53:52 -0900

    clean out unsused states from provider state class

--- lib/presentation/sims/provider/sims_provider.dart
+++ lib/presentation/sims/provider/sims_provider.dart
@@ -1,4 +1,3 @@
-import 'package:bit_connect/presentation/sims/model/general_status.dart';
 import 'package:bit_connect/presentation/sims/model/sims_std.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -10,10 +9,6 @@ class SIMSProvider extends ChangeNotifier {
   bool _isStatusWithLogin = true;
 
   Student? _loggedInStd;
-
-  List<GeneralStatus> _generalStatus = [];
-  bool _isLoading = false;
-  String _error = "";
 
   void setPreviousIndex(int index) {
     _previousIndex = index;
@@ -40,30 +35,11 @@ class SIMSProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  void setGeneralStatus(List<GeneralStatus> generalStatus) {
-    _generalStatus = generalStatus;
-    notifyListeners();
-  }
-
-  void setError(error) {
-    _error = error;
-    notifyListeners();
-  }
-
-  void setIsLoaeing(bool truthValue) {
-    _isLoading = truthValue;
-    notifyListeners();
-  }
-
   int get previousIndex => _previousIndex;
   bool get isLoginCanceled => _isLoginCanceled;
   bool get isUserLoggedInBefore => _isUserLoggedInBefore;
   bool get isStatusWithLogin => _isStatusWithLogin;
   Student? get loggedInStd => _loggedInStd;
-  String get error => _error;
-  bool get isLoading => _isLoading;
-
-  List<GeneralStatus> get generalStatus => _generalStatus;
 
   void cancelLogin() {
     _isLoginCanceled = true;
